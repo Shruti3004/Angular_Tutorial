b1af769be3193b1e1b6519e02d44c9ddb464850b
Shruti3004 <mail2shruti.ag@gmail.com>
2021-03-27 12:38:39 +0530

    Added directives

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { AdminComponent } from './admin/admin.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CustomStyleDirective } from './custom-style.directive';
 
 @NgModule({
-  declarations: [AppComponent, UserComponent, AdminComponent, PageNotFoundComponent],
+  declarations: [AppComponent, UserComponent, AdminComponent, PageNotFoundComponent, CustomStyleDirective],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
