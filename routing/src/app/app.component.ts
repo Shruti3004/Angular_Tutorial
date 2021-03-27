81c1dcdb4b9ac7186904776d6a1dc764ae879b42
Shruti3004 <mail2shruti.ag@gmail.com>
2021-03-27 13:25:03 +0530

    Added Interface

--- routing/src/app/app.component.ts
+++ routing/src/app/app.component.ts
@@ -3,6 +3,13 @@ import { UserComponent } from './user/user.component';
 import { UsersDataService } from './users-data.service';
 import {UsersServiceService} from'./users-service.service';
 
+interface dataType{
+  name: string,
+  id: number,
+  indian: boolean,
+  address: any
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,16 +18,25 @@ import {UsersServiceService} from'./users-service.service';
 export class AppComponent {
   title = 'routing';
   name  =  '';
-  data : any = [];
+  // data : any = [];
   // constructor(private user: UsersDataService) {
   //   console.log(this.user.getData);
   //   let data=this.user.getData();
   //   this.name=data.name;
   // }
-  constructor(private user:UsersServiceService){
-    this.user.getData().subscribe(data => {
-      console.log(data);
-      this.data = data;
-    })
+  // constructor(private user:UsersServiceService){
+  //   this.user.getData().subscribe(data => {
+  //     console.log(data);
+  //     this.data = data;
+  //   })
+  // }
+  getData(){
+    const data:dataType={
+      name: 'anil kumar',
+      id: 100,
+      indian: true,
+      address: '39, Civil LInes'
+    }
+    return data;
   }
 }
